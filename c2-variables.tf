6e2b924f10e345187330740d31c50eda00faa2ff
Ubuntu <ubuntu@ip-172-31-58-8.ec2.internal>
2022-02-02 12:36:26 +0000

    Terraform files First commit

---
+++ c2-variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+    description = " REgion in which AWS Resources to be created"
+    type = string
+}
+
+variable "instance_type" {
+    description = "EC2 instance type - Instance sizing"
+    type = string
+}
