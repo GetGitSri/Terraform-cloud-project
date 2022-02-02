c52252caea634c3a8e621775bfa04ac6d08737f9
Ubuntu <ubuntu@ip-172-31-58-8.ec2.internal>
2022-02-02 16:51:57 +0000

    added value to variable aws-region

--- c2-variables.tf
+++ c2-variables.tf
@@ -1,9 +1,11 @@
 variable "aws_region" {
     description = " REgion in which AWS Resources to be created"
     type = string
+    default = us-east-1
 }
 
 variable "instance_type" {
     description = "EC2 instance type - Instance sizing"
     type = string
+    
 }
